d6bd8c58292b54ce23ccabf0ea279363c33f8082
Giu3Ga <giuliasouza366@gmail.com>
2024-08-08 09:50:00 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,5 +2,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
-st.title("Hello, Gabriel e Giulia")
-
+st.title("Olá, Gabriel e Giulia!")
+st.header("Turma 2EI-S")
+st.subheader("Sou Santista nato")
+st.write("Lets go")
